a08aeaa66d92f3dcbb3de974a33989de4a498661
Dmitry Smirnov <dmitrijs.smirnovs@diatomenterprises.com>
2017-05-30 15:13:05 +0300

    Add project files.

---
+++ WebAppOwin/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.ApplicationInsights.Extensibility;
+using System.Diagnostics;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace WebAppOwin
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            DisableApplicationInsightsOnDebug();
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        /// <summary>
+        /// Disables the application insights locally.
+        /// </summary>
+        [Conditional("DEBUG")]
+        private static void DisableApplicationInsightsOnDebug()
+        {
+            TelemetryConfiguration.Active.DisableTelemetry = true;
+        }
+    }
+}
